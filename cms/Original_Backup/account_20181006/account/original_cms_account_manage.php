2f11f6cc0909366356fb551a1319bf532eefeb03
james <james20041135@gmail.com>
2018-10-06 13:13:50 +0800

    add cms tamplates

--- cms/Original_Backup/account_20181006/account/original_cms_account_manage.php
+++ cms/Original_Backup/account_20181006/account/original_cms_account_manage.php
@@ -3,86 +3,27 @@
  $query = "SELECT * FROM TBUSER ORDER BY id DESC";  
  $result = mysqli_query($connect, $query);  
  ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Content Management Systems</title>  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
-<!DOCTYPE html>
-<html>
+<!-- DataTables -->
+<link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet" />
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 
-<head>
+  
+  
+      </head>  
+      <body>  
+ 
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>INSPINIA | Minor view</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
-
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-
-</head>
-
-<body>
-
-<div id="wrapper">
-
-    <nav class="navbar-default navbar-static-side" role="navigation">
-        <div class="sidebar-collapse">
-            <ul class="nav metismenu" id="side-menu">
-                <li class="nav-header">
-                    <div class="dropdown profile-element">
-                            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David Williams</strong>
-                             </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
-                            <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                                <li><a href="#">Logout</a></li>
-                            </ul>
-                    </div>
-                    <div class="logo-element">
-                        IN+
-                    </div>
-                </li>
-                <li>
-                    <a href="index.php"><i class="fa fa-th-large"></i> <span class="nav-label">Main view</span></a>
-                </li>
-                <li class="active">
-                    <a href="cms_account_manage.php"><i class="fa fa-th-large"></i> <span class="nav-label">Minor view</span> </a>
-                </li>
-            </ul>
-
-        </div>
-    </nav>
-
-    <div id="page-wrapper" class="gray-bg">
-        <div class="row border-bottom">
-            <nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
-                <div class="navbar-header">
-                    <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
-                   <!-- <form role="search" class="navbar-form-custom" method="post" action="#">
-                        <div class="form-group">
-                            <input type="text" placeholder="Search for something..." class="form-control" name="top-search" id="top-search">
-                        </div>
-                    </form>-->
-                </div>
-                <ul class="nav navbar-top-links navbar-right">
-                    <li>
-                        <a href="#">
-                            <i class="fa fa-sign-out"></i> Log out
-                        </a>
-                    </li>
-                </ul>
-
-            </nav>
-        </div>
-        <div class="wrapper wrapper-content animated fadeInRight">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="text-Left m-t-lg">
-
-                    
-  <!--start-->
-           <div class="container" style="width:100%;">
-                <h3 align="center">Account Management</h3>  
+          <br /><br />
+           <div class="container" style="width:1000px;">  
+                <h3 align="center">Content Management Systems | Account Management</h3>  
                 <br />  
                 <div class="table-responsive">  
                      <div>  
@@ -136,42 +77,8 @@
                      </div>  
                 </div>  
            </div>  
-
-           <!--end-->
-
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer">
-            <div class="pull-right">
-                10GB of <strong>250GB</strong> Free.
-            </div>
-            <div>
-                <strong>Copyright</strong> Example Company &copy; 2014-2017
-            </div>
-        </div>
-
-    </div>
-</div>
-
-<!-- Mainly scripts -->
-<script src="js/jquery-3.1.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-<script src="js/plugins/dataTables/datatables.min.js"></script>
-<script src="js/plugins/dataTables/dataTables.bootstrap4.min.js"></script>
-
-
-<!-- Custom and plugin javascript -->
-<script src="js/inspinia.js"></script>
-<script src="js/plugins/pace/pace.min.js"></script>
-
-</body>
-</html>
-
+      </body>  
+ </html>  
 
 
   <!-- Add Form & Edit Form-->
@@ -236,8 +143,8 @@
  </div> 
 <!-- End Add Form and Edit Form-->
 
+ <script>  
 
-<script>
  $(document).ready(function(){  
       $('#add').click(function(){  
            $('#insert').val("Insert");  
@@ -354,7 +261,8 @@ $(document).on('click', '.delete_data', function(){
    return false; 
   }
  });
- 
+
+
    	$( ".table" ).DataTable({
 bPaginate: true,
 bLengthChange: false,
@@ -364,5 +272,4 @@ bInfo: false,
 bAutoWidth: false,
 pageLength: 10
   	});
-
-</script>
+ </script>
