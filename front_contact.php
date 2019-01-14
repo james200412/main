67674a06a183e728353c6e90cdd3068b6119544f
james200412 <james20041135@gmail.com>
2019-01-14 21:05:30 +0800

    change footer and addthis add-on

--- front_contact.php
+++ front_contact.php
@@ -1,6 +1,3 @@
-<!-- Go to www.addthis.com/dashboard to customize your tools --> 
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b921927bca7c3ad"></script>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -85,11 +82,11 @@
 											<p>Tat Chee Avenue. Kowloon Tong</p>
 							   			</address>
 							   		</div>
-									</div>	
+									</div>
+									
+										
 									<div class="follow-us">
-										<h3>follow us on</h3>
-										<!-- Go to www.addthis.com/dashboard to customize your tools --> 
-										<div class="addthis_inline_share_toolbox"></div> 
+	
 									</div>
 			
 							
--- include/front_footer.php
+++ include/front_footer.php
@@ -7,29 +7,55 @@ if(isset($_SESSION["userid"]) && $_SESSION['userlevel'] == 0){
 			<div class="container">
 				<div class="contact-section-grids">
 					<div class="col-md-3 contact-section-grid wow fadeInLeft" data-wow-delay="0.4s">
-						<h4>Menu</h4>
+						<h4>About Us</h4>
 						<ul>
-							<li><i class="point"></i></li>
-							<li class="data"><a href="front_menu.php">Menu</a></li>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+						<li class="data"><a href="index.php">Brand Story</a></li>
 						</ul>
-						
+						<ul>
+							<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="front_contact.php">Contact Us</a></li>
+						</ul>
 					</div>
 					<div class="col-md-3 contact-section-grid wow fadeInLeft" data-wow-delay="0.4s">
-						<h4>Company Info</h4>
+						<h4>The Restaurant</h4>
 						<ul>
-							<li><i class="point"></i></li>
-							<li class="data"><a href="#">About Us</a></li>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="front_menu.php">Menu</a></li>
 						</ul>
+
 						<ul>
-							<li><i class="point"></i></li>
-							<li class="data"><a href="#">Contact Us</a></li>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="index.php">Privacy Policy</a></li>
 						</ul>
+					</div>
+					<div class="col-md-3 contact-section-grid wow fadeInLeft" data-wow-delay="0.4s">
+						<h4>Member</h4>
 						<ul>
-							<li><i class="point"></i></li>
-							<li class="data"><a href="#">Privacy Policy</a></li>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="front_userinfo.php">Account Info</a></li>
+						</ul>
+
+						<ul>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="front_viewcart.php">Cart</a></li>
+						</ul>
+
+						<ul>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="front_order.php">Track Order</a></li>
 						</ul>
 					</div>
 				
+					<div class="col-md-3 contact-section-grid wow fadeInLeft" data-wow-delay="0.4s">
+					<h4>Share With Friend!</h4>
+					<!-- Go to www.addthis.com/dashboard to customize your tools --> 
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b921927bca7c3ad"></script>
+
+						<!-- Go to www.addthis.com/dashboard to customize your tools --> 
+					<div class="addthis_inline_share_toolbox"></div> 					
+					</div>
+
 				</div>
 			</div>
 		</div>
@@ -55,29 +81,58 @@ if(isset($_SESSION["userid"]) && $_SESSION['userlevel'] == 0){
 			<div class="container">
 				<div class="contact-section-grids">
 					<div class="col-md-3 contact-section-grid wow fadeInLeft" data-wow-delay="0.4s">
-						<h4>Menu</h4>
+						<h4>About Us</h4>
 						<ul>
-							<li><i class="point"></i></li>
-							<li class="data"><a href="front_menu.php">Menu</a></li>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+						<li class="data"><a href="index.php">Brand Story</a></li>
 						</ul>
-						
+						<ul>
+							<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="front_contact.php">Contact Us</a></li>
+						</ul>
 					</div>
 					<div class="col-md-3 contact-section-grid wow fadeInLeft" data-wow-delay="0.4s">
-						<h4>Company Info</h4>
+						<h4>The Restaurant</h4>
 						<ul>
-							<li><i class="point"></i></li>
-							<li class="data"><a href="#">About Us</a></li>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="front_menu.php">Menu</a></li>
 						</ul>
+
 						<ul>
-							<li><i class="point"></i></li>
-							<li class="data"><a href="#">Contact Us</a></li>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="index.php">Privacy Policy</a></li>
 						</ul>
+					</div>
+					<div class="col-md-3 contact-section-grid wow fadeInLeft" data-wow-delay="0.4s">
+						<h4>Member</h4>
 						<ul>
-							<li><i class="point"></i></li>
-							<li class="data"><a href="#">Privacy Policy</a></li>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="front_login.php">Login</a></li>
+						</ul>
+
+						<ul>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="register.php">Register</a></li>
+						</ul>
+
+						<ul>
+						<li><i class="glyphicon glyphicon-tag" style="color:green"></i></li>
+							<li class="data"><a href="front_order.php">Track Order</a></li>
 						</ul>
 					</div>
 				
+
+
+					<div class="col-md-3 contact-section-grid wow fadeInLeft" data-wow-delay="0.4s">
+					<h4>Share With Friend!</h4>
+					<!-- Go to www.addthis.com/dashboard to customize your tools --> 
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b921927bca7c3ad"></script>
+
+						<!-- Go to www.addthis.com/dashboard to customize your tools --> 
+					<div class="addthis_inline_share_toolbox"></div> 					
+					</div>
+
+
 				</div>
 			</div>
 		</div>
@@ -97,3 +152,5 @@ if(isset($_SESSION["userid"]) && $_SESSION['userlevel'] == 0){
 }
 
 ?>
+
+										
